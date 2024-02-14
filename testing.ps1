824c690a6520129a9fbaec833d1bd044c536f362
Vinodh <51962718+Tumma123@users.noreply.github.com>
2024-02-14 12:37:37 +0530

    Add files via upload

---
+++ testing.ps1
@@ -0,0 +1,10 @@
+﻿
+$arguments = "/i `"$webDeployInstallerFilePath`" /quiet"
+Start-Process msiexec.exe -ArgumentList $arguments -Wait
+
+foreach($_msiFiles in 
+($_msiFiles = Get-ChildItem $_Source -Recurse | Where{$_.Extension -eq ".msi"} |
+ Where-Object {!($_.psiscontainter)} | Select-Object -ExpandProperty FullName)) 
+{
+    msiexec /i $_msiFiles /passive
+}
